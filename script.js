6afd88f78e232413d84bee1ac13d46da1bc0bdde
Dida En <den.elfsuju@gmail.com>
2021-01-18 15:05:03 +0700

    Menambahkan file script.js, soal, dan array awal

---
+++ script.js
@@ -0,0 +1,12 @@
+// Pengelolaan angkot
+// Sopirnya siapa
+// Trayek jalurnya
+// Kas atau uang pembayaran angkot
+// Penumpang
+
+var angkot1 = {
+    sopir : 'Slamet Raharjo',
+    trayek : 'Batang Kota - Subah',
+    penumpang : []
+    kas : 0
+}
