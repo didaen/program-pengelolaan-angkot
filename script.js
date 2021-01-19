382fd1bea85e493d1be706bd495fcde533159f3a
Dida En <den.elfsuju@gmail.com>
2021-01-20 03:00:26 +0700

    Membuat object dengan cara constructor

--- script.js
+++ script.js
@@ -4,12 +4,34 @@
 // Kas atau uang pembayaran angkot
 // Penumpang
 
-var angkot1 = {
-    sopir : 'Slamet Raharjo',
-    trayek : 'Batang Kota - Subah',
-    penumpang : [],
-    kas : 0,
-    tambahPenumpang : penumpangNaik() {
+function Angkot(supir, trayek, penumpang, kas) {
+    // Property dan method yang dibutuhkan
+    this.supir = supir;
+    this.trayek = trayek;
+    this.penumpang = penumpang;
+    this.kas = kas; 
 
+    this.penumpangNaik = function(namaPenumpang) {
+        this.penumpang.push(namaPenumpang);
+        return this.penumpang;
+    }
+
+    this.penumpangTurun = function (namaPenumpang, bayar) {
+        if( this.penumpang.length === 0 ) {
+            alert('Angkot masih kosong!');
+            return false;
+        }
+
+        for( var i = 0; i < this.penumpang.length; i++ ) {
+            if( this.penumpang[i] === namaPenumpang ) {
+                this.penumpang[i] = undefined;
+                this.kas += bayar;
+                return this.penumpang;
+            }
+        }
     }
 }
+
+var angkot1 = new Angkot('Dida', ['Kendal', 'Sukodono'], [], 0);
+
+var angkot2 = new Angkot('Fina', ['Wonosobo', 'Dieng'], [], 0);
